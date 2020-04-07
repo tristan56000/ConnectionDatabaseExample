ffd88c2c1ee304388a1488a133fac684e364c1ab
Tristan Guerin <56036297+tristan56000@users.noreply.github.com>
2020-04-07 11:12:48 +0200

    add attribute test class

--- src/main/java/fr/ensibs/database/Test.java
+++ src/main/java/fr/ensibs/database/Test.java
@@ -4,12 +4,26 @@ public class Test {
 
     private String a;
 
-    public Test(String a){
+    private int b;
+
+    public Test(String a, int b){
         this.a = a;
+        this.b= b;
     }
 
     public String getA(){
         return a;
     }
 
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
 }
